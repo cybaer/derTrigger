a6c77d9eb15f73268d8045c71ed9df14d17c6536
cybaer <cybaer@MacBaer>
2017-02-21 07:53:51 +0100

    clean up

--- MCP23017.h
+++ MCP23017.h
@@ -65,17 +65,10 @@ public:
 
    Wire.begin();
 
-   // set defaults!
-   // all inputs on port A and B
+   // port A and B all outputs
    writeRegister(MCP23017_IODIRA,0);
    writeRegister(MCP23017_IODIRB,0);
  }
-
-
-  void pinMode(uint8_t p, uint8_t d);
-  void digitalWrite(uint8_t p, uint8_t d);
-  void pullUp(uint8_t p, uint8_t d);
-  uint8_t digitalRead(uint8_t p);
 
   void writeGPIOAB(uint16_t ba)
   {
@@ -99,21 +92,9 @@ public:
     Wire.write(data);
     Wire.endTransmission();
   }
-  uint16_t readGPIOAB();
-  uint8_t readGPIO(uint8_t b);
-
-  void setupInterrupts(uint8_t mirroring, uint8_t open, uint8_t polarity);
-  void setupInterruptPin(uint8_t p, uint8_t mode);
-  uint8_t getLastInterruptPin();
-  uint8_t getLastInterruptPinValue();
 
  private:
   uint8_t i2caddr;
-
-  uint8_t bitForPin(uint8_t pin);
-  uint8_t regForPin(uint8_t pin, uint8_t portAaddr, uint8_t portBaddr);
-
-  uint8_t readRegister(uint8_t addr);
 
   void writeRegister(uint8_t regAddr, uint8_t regValue)
   {
@@ -123,13 +104,6 @@ public:
     Wire.write(regValue);
     Wire.endTransmission();
   }
-
-  /**
-   * Utility private method to update a register associated with a pin (whether port A/B)
-   * reads its value, updates the particular bit, and writes its value.
-   */
-  void updateRegisterBit(uint8_t p, uint8_t pValue, uint8_t portAaddr, uint8_t portBaddr);
-
 };
 
 
